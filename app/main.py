adb407835f5faef026cc33dd22fe62eff978ed25
kgsgun <kgsgunpro@gmail.com>
2026-07-05 14:26:20 +0530

    first time deployed in fastapi cloud

--- app/main.py
+++ app/main.py
@@ -4,10 +4,5 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
