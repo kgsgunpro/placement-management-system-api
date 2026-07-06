75193fa74ce8d665707fc656a58aec479ada18d5
kgsgun <kgsgunpro@gmail.com>
2026-07-06 19:42:09 +0530

    minimal CRUD operations done

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from app.routers import students
-app = FastAPI()
+
+
+app = FastAPI( title="Placement Management System API",
+    description="Backend API for managing students and placements",
+    version="0.1.0")
+
+
 app.include_router(students.router)
 
 @app.get("/")
--- app/routers/students.py
+++ app/routers/students.py
@@ -7,6 +7,8 @@ router=APIRouter(prefix='/students' , tags=['Students'])
 class NewStudent(BaseModel):    
     name:str
     class_no:int
+
+
 students_db=[{'id' : 1 , 'name':'ravi' , 'class_no' :10}]
 
 
@@ -28,3 +30,20 @@ async def create_student(student : NewStudent):
     new_entry = {'id':id , 'name' : student.name , 'class_no': student.class_no}
     students_db.append(new_entry)
     return new_entry
+
+@router.put('/{id}')
+async def update_student(id:int ,student : NewStudent) :
+    for students in students_db :
+        if id == students['id']:
+            students.update({'name': student.name , 'class' : student.class_no})
+            return students
+    return HTTPException(status_code=404 , detail = 'Student not found')
+
+@router.delete('/{id}')
+async def delete_student(id:int):
+    for students in students_db:
+        if id ==students['id']:
+            temp=students
+            students_db.remove(students)
+            return temp
+    return HTTPException(status_code=404 , detail='Student not found')
