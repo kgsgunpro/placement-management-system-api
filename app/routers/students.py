7644ea697ca3b9dfa3d6768c186ff8200c8e55e4
kgsgun <kgsgunpro@gmail.com>
2026-07-07 19:42:00 +0530

    CRUD operations completes

--- app/routers/students.py
+++ app/routers/students.py
@@ -1,28 +1,39 @@
 from fastapi import FastAPI ,HTTPException , APIRouter
 from pydantic import BaseModel
+from typing import Annotated
 
 router=APIRouter(prefix='/students' , tags=['Students'])
 
 
-class NewStudent(BaseModel):    
+class NewStudent(BaseModel):
     name:str
     class_no:int
 
 
-students_db=[{'id' : 1 , 'name':'ravi' , 'class_no' :10}]
+students_db : list[dict]=[{'id' : 1 , 'name':'ravi' , 'class_no' :10}]
 
-
+def not_found(m):
+    raise HTTPException(status_code=404 , detail=m)
 
 @router.get('/')
-async def get_students():
+async def get_students(classno : Annotated[int | None , ' get students by class'] = None):
+    if classno is not None :
+        temp=[]
+        for student in students_db :
+            if classno == student['class_no']:
+                temp.append(student)
+        if not temp:
+            not_found('Students not found')
+        return temp
+
     return students_db
 
 @router.get('/{id}')
-async def get_student_by_id(id:int):
+async def get_student_by_id(id:int ):
     for student in students_db:
         if student['id']==id:
             return student
-    raise HTTPException(status_code=404 , detail='student not found')
+    not_found('student not found')
 
 @router.post('/')
 async def create_student(student : NewStudent):
@@ -33,17 +44,17 @@ async def create_student(student : NewStudent):
 
 @router.put('/{id}')
 async def update_student(id:int ,student : NewStudent) :
-    for students in students_db :
-        if id == students['id']:
-            students.update({'name': student.name , 'class_no' : student.class_no})
-            return students
-    raise HTTPException(status_code=404 , detail = 'Student not found')
+    for db_student in students_db :
+        if id == db_student['id']:
+            db_student.update({'name': student.name , 'class_no' : student.class_no})
+            return db_student
+    not_found('student not found')
 
 @router.delete('/{id}')
 async def delete_student(id:int):
-    for students in students_db:
-        if id ==students['id']:
-            temp=students
-            students_db.remove(students)
-            return temp
-    return HTTPException(status_code=404 , detail='Student not found')
+    for student in students_db:
+        if id ==student['id']:
+            
+            students_db.remove(student)
+            return student
+    not_found('student not found')
